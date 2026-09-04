4020dd0ebc93261f6fdce17f978a42f6dd544ff2
Fralonso16 <fran.alonsodemedina@gmail.com>
2026-09-04 18:36:41 +0200

    Permitir CORS desde el frontend desplegado en Render

--- main.py
+++ main.py
@@ -27,7 +27,11 @@ app.add_middleware(
     # En un proyecto real, aqui pondrias el dominio exacto de tu frontend.
     # Para aprender y desarrollar en local, permitimos estos dos origenes
     # tipicos de un servidor de archivos estatico simple.
-    allow_origins=["http://localhost:5500", "http://127.0.0.1:5500"],
+    allow_origins=[
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://user-auth-api-frontend.onrender.com",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
